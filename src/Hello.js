c0ab8138802b675bf06ecd10fc7425864d62c5a4
daryl-san <99176560+daryl-san@users.noreply.github.com>
2022-04-21 19:17:06 +1000

    installed and imported 'tachyons' to index.js.
    edited index.css to alter look

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Hello extends Component {
     render() {
         return (
-            <div>
+            <div className='f1 tc'>
                 <h1> Hello World Matey!!</h1>
                 <p> Welcome to React!</p>
             </div>
