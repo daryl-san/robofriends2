2c72aed54062af2a0fdcf7d8eadc3dffd8ce0f8f
daryl-san <99176560+daryl-san@users.noreply.github.com>
2022-04-21 22:09:56 +1000

    added search function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from './robots';
 import SearchBox from "./SearchBox";
 
-const App = () => {
-    return (
-        <div>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
 
-    );
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    //always have non-native functions to be formatted as  
+    //per below to avoid 'undefined state' error
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        // console.log(filteredRobots);
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+        })
+        return (
+            <div>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div >
+        );
+    }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
     return (
         <div className='pa2 tc'>
             <input
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search'
-                placeHolder='search robots'
+                placeholder='search robots'
+                onChange={searchChange}
             />
         </div>
     );
