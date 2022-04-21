c2782dfd0e7d534f263778f6fc7acef39f311702
daryl-san <99176560+daryl-san@users.noreply.github.com>
2022-04-21 19:47:04 +1000

    introduced props

--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 
-class Hello extends Component {
-    render() {
-        return (
-            <div className='f1 tc'>
-                <h1> Hello World Matey!!</h1>
-                <p> Welcome to React!</p>
-            </div>
-        )
-    }
+// class Hello extends Component {
+//     render() {
+// return (
+//     <div className='f1 tc'>
+//         <h1> Hello World Matey!!</h1>
+//         <p>{this.props.greeting}</p>
+//     </div>
+// )
+//     }
+// }
+
+const Hello = (props) => {
+    return (
+        <div className='f1 tc'>
+            <h1> Hello World Matey!!</h1>
+            <p>{props.greeting}</p>
+        </div>
+    )
 }
 
 export default Hello;
